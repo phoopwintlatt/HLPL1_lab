9e97fcf99928052662a8f935cab10916ba7c6ad8
phoopwintlatt <66958679+phoopwintlatt@users.noreply.github.com>
2022-04-26 18:21:17 +0200

    Update ch8_drill2.cpp

--- week01/ch8_drill2.cpp
+++ week01/ch8_drill2.cpp
@@ -1,4 +1,4 @@
-#include "std_lib_facilities.h"
+#include "../std_lib_facilities.h"
 
 void swap_v(int a,int b)
 {
@@ -30,18 +30,20 @@ int main()
 	int y =9;
 	swap_v(x,y);
 	cout << x << " " << y << "\n";
-	swap_cr(7,9);
+	swap_r(x,y);
 	cout << x << " " << y << "\n";
 	
 	const int cx = 7;
 	const int cy = 9;
-	swap_v(cx,cy);
-	swap_cr(7.7,9.9);
+	swap_cr(cx,cy);
+	cout << x << " " << y << "\n";
+	
+	swap_v(7.7,9.9);
 
 	double dx = 7.7;
 	double dy = 9.9;
-	swap_v(dx,dy);
-	swap_cr(7.7,9.9);
+	swap_r(dx,dy);
+	cout << dx << " " << dy << "\n";
 	
 	return 0;
 }
