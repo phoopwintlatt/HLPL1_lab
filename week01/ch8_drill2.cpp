9c678d38a9097c94bc80a1862feb3e91c4a693f9
phoopwintlatt <66958679+phoopwintlatt@users.noreply.github.com>
2022-05-09 04:03:50 +0200

    Update ch8_drill2.cpp

--- week01/ch8_drill2.cpp
+++ week01/ch8_drill2.cpp
@@ -1,49 +1,55 @@
 #include "../std_lib_facilities.h"
 
-void swap_v(int a,int b)
-{
+void swap_v(int a, int b) {
 	int temp;
 	temp = a;
 	a = b;
-	b=temp;
+	b = temp;
+
 }
 
-void swap_r(int& a,int& b)
+void swap_r(int &a, int &b)
 {
 	int temp;
 	temp = a;
 	a = b;
-	b=temp;
+	b = temp;
 }
 
-void swap_cr(const int& a,const int& b)
+/*void swap_cr(const int& a, const int& b)
 {
-	int temp;
-	temp = a;
+	int temp = a;
 	a = b;
-	b=temp;
+	b = temp;
+
+}*/
+
+void swap_cr(const int& ca, const int& cb)
+{
+    int a, b;
+    a = cb;
+    b = ca;
 }
 
 int main()
 {
 	int x = 7;
-	int y =9;
-	swap_v(x,y);
-	cout << x << " " << y << "\n";
-	swap_r(x,y);
-	cout << x << " " << y << "\n";
-	
+	int y = 9;
+	swap_r(x, y);
+	cout << x << "  " << y << endl;
+	swap_cr(x, y);
+	cout << x << "  " << y << endl;
 	const int cx = 7;
 	const int cy = 9;
-	swap_cr(cx,cy);
-	cout << x << " " << y << "\n";
-	
-	swap_v(7.7,9.9);
 
+	swap_cr(cx, cy);
+	cout << cx << "  " << cy << endl;
+	swap_v(7.7, 9.9);
+	
+	
 	double dx = 7.7;
 	double dy = 9.9;
-	swap_r(dx,dy);
-	cout << dx << " " << dy << "\n";
-	
+	swap_cr(dx, dy);
+	cout << dx << "  " << dy << endl;
 	return 0;
 }
